10103a4318fe413d13fabbad38197cb05219e7c8
Larissa Barbosa <larissabarbosa@id.uff.br>
2022-06-17 12:12:16 -0300

    console.table property

---
+++ main.js
@@ -0,0 +1,16 @@
+const fruits = [
+    {
+        name: 'Banana',
+        quantity: 24,
+    },
+    {
+        name: 'Strawberry',
+        quantity: 5,
+    },
+    {
+        name: 'Grape',
+        quantity: 39,
+    },
+];
+
+console.table(fruits);
